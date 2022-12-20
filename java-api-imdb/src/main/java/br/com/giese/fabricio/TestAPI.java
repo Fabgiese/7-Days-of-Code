98e3dc38029c65ecb8b3a2f7711fd6fc0851221b
FabGiese <fabgiese@gmail.com>
2022-12-19 22:04:25 -0300

    Second day of 7DaysOfCode with java. Parsing Json and print it out in console.

--- java-api-imdb/src/main/java/br/com/giese/fabricio/TestAPI.java
+++ java-api-imdb/src/main/java/br/com/giese/fabricio/TestAPI.java
@@ -1,7 +1,6 @@
 package br.com.giese.fabricio;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -9,33 +8,65 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Properties;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParser;
 
 public class TestAPI {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException, URISyntaxException, InterruptedException {
+	public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
 		
 		Properties apiKey = new Properties();
 		String filePath = "./src/main/resources/config.properties";
 		apiKey.load(new FileInputStream(filePath));
 		
-		String search = "/SpiderMan";
 		String key = "/" + apiKey.getProperty("apiKey");
-		String uriSearch = "https://imdb-api.com/en/API/Search";
-		String uriCompleta = uriSearch + key + search;
+		String uriSearch = "https://imdb-api.com/en/API/Top250Movies";
+		String uriCompleta = uriSearch + key;
 		
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder().GET().uri(new URI(uriCompleta)).build();
 		
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 		
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		System.out.println(gson.toJson(JsonParser.parseString(response.body())));
+		Map<Integer, Map<String, String>> jsonParseado = parseJson(response);
 		
+		for(int i = 0; i < jsonParseado.size(); i++) {
+			Map<String,String> mapTemp = jsonParseado.get(i);
+			for (Entry<String, String> es : mapTemp.entrySet()) {
+				System.out.println(es.getKey() + " : " + es.getValue());
+			}
+			System.out.println("-----------------------");
+			System.lineSeparator();
+		}
+	}
+
+	private static Map<Integer, Map<String, String>> parseJson(HttpResponse<String> response) {
+		String[] json = response.body()
+				.replace("[", "")
+				.replace("]", "")
+				.replace("{", "")
+				.replace("},", "/@/")
+				.replace("\"items\":", "")
+				.replace("\"errorMessage\":\"\"}", "")
+				.replace("\",\"", "/@@/")
+				.split("/@/");
+		
+		Map<Integer, Map<String, String>> jsonParseado = new HashMap<>();
+		
+		for (int i = 0; i < json.length; i++) {
+			final int index = i;
+			jsonParseado.put(i, new HashMap<>());
+			String[] keyValueTogether = json[index].substring(1, json[index].length() - 1) .split("/@@/");
+			List<String[]> keyValue = new ArrayList<>();
+			Arrays.asList(keyValueTogether).forEach(s -> keyValue.add(s.split("\":\"")));
+			keyValue.forEach(kv -> jsonParseado.get(index).put(kv[0], kv[1]));
+		}
+		return jsonParseado;
 	}
 
 }
